c370d348f0928ccf41f1add8c2f39c970a5303b2
loose morgan <loose.morgan@laposte.net>
2021-12-14 11:49:22 +0100

    création des composants

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PereNoelComponent } from './pere-noel/pere-noel.component';
-import { EnfantComponent } from './enfant/enfant.component';
 import { CadeauCardComponent } from './components/cadeau-card/cadeau-card.component';
 import { CadeauListComponent } from './components/cadeau-list/cadeau-list.component';
 import { CadeauxComponent } from './pages/cadeaux/cadeaux.component';
@@ -12,8 +10,6 @@ import { CadeauxComponent } from './pages/cadeaux/cadeaux.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PereNoelComponent,
-    EnfantComponent,
     CadeauCardComponent,
     CadeauListComponent,
     CadeauxComponent
